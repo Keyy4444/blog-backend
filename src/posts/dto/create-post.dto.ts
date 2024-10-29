4fe95ccc31a47d6d8332a7fd45ea6214a4b11359
Oleksii <oleksprokopchenko@gmail.com>
2024-10-29 08:05:55 +0200

    implemented swagger

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,20 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({
+    example: 'My First Post',
+    description: 'The title of the post',
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
   slug?: string;
 
+  @ApiProperty({
+    example: 'An introduction to my first post',
+    description: 'A short description of the post',
+  })
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty({
+    example: 'Detailed content of the post',
+    description: 'The main content of the post in WYSIWYG format',
+  })
   @IsString()
   @IsNotEmpty()
   content: string;
 
+  @ApiProperty({
+    example: 'https://example.com/banner.jpg',
+    description: 'URL of the banner image for the post',
+  })
   @IsString()
   bannerImage: string;
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,17 +10,39 @@ import {
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto, UpdatePostDto } from './dto';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Posts')
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all posts' })
+  @ApiResponse({ status: 200, description: 'Returns an array of all posts' })
   getAllPosts() {
     return this.postsService.getAllPosts();
   }
 
   @Get('/pagination')
+  @ApiOperation({ summary: 'Get paginated posts' })
+  @ApiQuery({ name: 'page', required: false, description: 'Page number' })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    description: 'Number of items per page',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns paginated list of posts and number of total pages',
+  })
   getPaginatedPosts(
     @Query('page') page: number,
     @Query('limit') limit: number,
@@ -29,6 +51,19 @@ export class PostsController {
   }
 
   @Get('/search')
+  @ApiOperation({ summary: 'Search posts by title' })
+  @ApiQuery({ name: 'title', description: 'Title to search for' })
+  @ApiQuery({ name: 'page', required: false, description: 'Page number' })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    description: 'Number of items per page',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Returns paginated posts matching the search query and a number of total pages',
+  })
   searchPostsByTitle(
     @Query('title') title: string,
     @Query('page') page: number,
@@ -38,26 +73,42 @@ export class PostsController {
   }
 
   @Get('slug/:slug')
+  @ApiOperation({ summary: 'Get post by slug' })
+  @ApiParam({ name: 'slug', description: 'Slug of the post' })
+  @ApiResponse({ status: 200, description: 'Returns a single post by slug' })
   getPostBySlug(@Param('slug') slug: string) {
     return this.postsService.getPostBySlug(slug);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get post by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the post' })
+  @ApiResponse({ status: 200, description: 'Returns a single post by ID' })
   getPostById(@Param('id') id: string) {
     return this.postsService.getPostById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new post' })
+  @ApiBody({ type: CreatePostDto })
+  @ApiResponse({ status: 201, description: 'The post has been created' })
   createPost(@Body() createPostDto: CreatePostDto) {
     return this.postsService.createPost(createPostDto);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a post' })
+  @ApiParam({ name: 'id', description: 'ID of the post' })
+  @ApiBody({ type: UpdatePostDto })
+  @ApiResponse({ status: 200, description: 'The post has been updated' })
   updatePost(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postsService.updatePost(id, updatePostDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a post' })
+  @ApiParam({ name: 'id', description: 'ID of the post' })
+  @ApiResponse({ status: 200, description: 'The post has been deleted' })
   deletePost(@Param('id') id: string) {
     return this.postsService.deletePost(id);
   }
